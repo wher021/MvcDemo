c7af666416b19f6cf53e742213293b3b55cc01e4
Herrera Willy <willy.herrera@scania.com>
2018-09-12 16:39:26 +0200

    second commit

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using Client.HelloWorldServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldClient client = new HelloWorldClient("BasicHttpBinding_IHelloWorld");
+            Name person = new Name();
+            person.First = "willy";
+            person.Last = "herrera";
+            Console.WriteLine(client.SayHello(person));
+            Console.ReadLine();
+        }
+    }
+}
